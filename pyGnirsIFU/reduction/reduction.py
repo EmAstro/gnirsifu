7d9aefe7df852f8ba4d2a46520c7b278d3fad5dc
EmAstro <emanuele.paolo.farina@gmail.com>
2022-05-26 00:56:25 -1000

    working on flats

--- pyGnirsIFU/reduction/reduction.py
+++ pyGnirsIFU/reduction/reduction.py
@@ -40,6 +40,7 @@ def reduce_flats(list_of_files, grating='111/mm_G5505', parameters=None):
     elif parameters is not None:
         parameters = parameters
     _clean_list_of_files(list_of_files, 'flat.lis')
+    _delete_files_in_list('flat.lis', prefix='n')
     gnirs.nsprepare('@flat.lis', shiftx='INDEF',shifty='INDEF')
     # nsreduce n @ flat.lis fl_cut + fl_nsappw - fl_sky - fl_dark - fl_flat -
 
