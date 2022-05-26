95071d16037d7cf2c8969bd61ca5a6523b6ceca4
EmAstro <emanuele.paolo.farina@gmail.com>
2022-05-25 23:55:02 -1000

    working on flats

--- pyGnirsIFU/reduction/reduction.py
+++ pyGnirsIFU/reduction/reduction.py
@@ -10,28 +10,13 @@ from pyGnirsIFU.reduction import parameters
 
 gnirs.nsheaders('gnirs')
 
+OBSTYPE = ['flat']
+
 
 class Reduction:
-    def __init__(self, file_list=[], processed_file_list=[], reduction_parameters=None):
-        self.file_list = file_list
-        self.processed_file_list = processed_file_list
+    def __init__(self, reduction_parameters=None, flat_list=[]):
         self.reduction_parameters = reduction_parameters
-
-    @property
-    def file_list(self):
-        return self._file_list
-
-    @file_list.setter
-    def file_list(self, file_list):
-        self._file_list = file_list
-
-    @property
-    def processed_file_list(self):
-        return self._processed_file_list
-
-    @processed_file_list.setter
-    def processed_file_list(self, processed_file_list):
-        self._processed_file_list = processed_file_list
+        self.flat_list = flat_list
 
     @property
     def reduction_parameters(self):
@@ -44,18 +29,30 @@ class Reduction:
         elif isinstance(reduction_parameters, parameters.Reduction):
             self._reduction_parameters = reduction_parameters
         else:
-            raise TypeError("epar not a parameters.Reduction object")
+            raise TypeError("reduction_parameters not a parameters.Reduction object")
 
-    def from_file(self, text_file):
+    @property
+    def flat_list(self):
+        return self._flat_list
+
+    @flat_list.setter
+    def flat_list(self, flat_list):
+        self._flat_list = flat_list
+
+    def from_file(self, text_file, file_type='flat'):
         data_table = ascii.read(text_file)
-        self.file_list = list(data_table['col1'].data)
+        file_list = list(data_table['col1'].data)
+        if file_type == 'flat':
+            self.flat_list = file_list
 
-    def purge_all_processed(self):
-        for processed_file in self.processed_file_list:
-            iraf.imdel(processed_file)
-            self.processed_file_list.remove(processed_file)
+    def purge_all_processed(self, file_type='flat'):
+        if file_type == 'flat':
+            for processed_file in self.processed_flat_list:
+                iraf.imdel(processed_file)
+                self.processed_flat_list.remove(processed_file)
 
 
+"""
 class Flats(Reduction):
     def __init__(self, file_list):
         super(self, file_list=file_list, ).__init__()
@@ -65,3 +62,4 @@ class Flats(Reduction):
             gnirs.nsprepare(self.flat_list, shiftx='INDEF', shifty='INDEF', logfile=self.epar.logfile)
     # nsprepare @flat.lis shiftx = INDEF shifty = INDEF nsreduce n @flat.lis fl_cut+ fl_nsappw- fl_sky- fl_dark- fl_flat-
     # nsflat rn@flat.lis darks = "" flatfile = "" darkfile = "" fl_save_dark + process = "fit" thr_flo = 0.15 thr_fup = 1.55
+"""
