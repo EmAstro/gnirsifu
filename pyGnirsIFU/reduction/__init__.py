900d22e282ba03a41e6cf82425159b4bf95692e7
EmAstro <emanuele.paolo.farina@gmail.com>
2022-05-25 23:10:40 -1000

    working on flats

--- pyGnirsIFU/reduction/__init__.py
+++ pyGnirsIFU/reduction/__init__.py
@@ -3,7 +3,12 @@ from pyraf.iraf import gemini
 from pyraf.iraf import gemtools
 from pyraf.iraf import gnirs
 
+from pyGnirsIFU.reduction import parameters
+from pyGnirsIFU.reduction import reduction
+
 iraf.unlearn(gemini)
 iraf.unlearn(gemtools)
 iraf.unlearn(gnirs)
+
+
 
