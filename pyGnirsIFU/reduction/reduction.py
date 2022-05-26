b50d172d09a09d2a438faa9b34f08753139bb751
EmAstro <emanuele.paolo.farina@gmail.com>
2022-05-26 00:55:10 -1000

    working on flats

--- pyGnirsIFU/reduction/reduction.py
+++ pyGnirsIFU/reduction/reduction.py
@@ -25,6 +25,15 @@ def _clean_list_of_files(list_of_files, list_name):
     list_file.close()
 
 
+def _delete_files_in_list(list_of_files, prefix=''):
+    data_table = ascii.read(list_of_files)
+    file_list = list(data_table['col1'].data)
+    for file_name in file_list:
+        file_name_cleaned = file_name.split['/'][:-2] + prefix + file_name.split['/'][-1]
+        if os.path.exists(file_name_cleaned):
+            shutil.move(file_name_cleaned, file_name_cleaned + '.backup')
+
+
 def reduce_flats(list_of_files, grating='111/mm_G5505', parameters=None):
     if (grating == '111/mm_G5505') & (parameters is None):
         parameters = DEFAULT_111mm_G5505_grating['FLAT']
