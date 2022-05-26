ec9cd72dbf824e9832528ea58fc7a5dc4ed7ae4f
EmAstro <emanuele.paolo.farina@gmail.com>
2022-05-26 00:45:37 -1000

    working on flats

--- pyGnirsIFU/scripts/gnirs_ifu_sort.py
+++ pyGnirsIFU/scripts/gnirs_ifu_sort.py
@@ -67,9 +67,7 @@ def main(args):
     root_filename = lists.from_list_to_string(args.root_filename)
     if not args.append:
         _remove_text_files(args.data_directory[0])
-    file_list = sorted(glob.glob(data_directory + "/" + root_filename + "/*.fits"))
-    from IPython import embed
-    embed()
+    file_list = sorted(glob.glob(data_directory + "/" + root_filename + "*.fits"))
     print("Sorting {} files for object: {}".format(len(file_list), object_name))
 
     for file_name in file_list:
